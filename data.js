b16e84c8697f1abb3edca42bc958b92484888ac8
Juvia Suisei <13936649+juviasuisei@users.noreply.github.com>
2025-07-14 10:21:32 -0400

    Add files via upload

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Main data aggregator - combines data from separate files
 window.checklistData = {
-  version: "0.18.2",
+  version: "0.12.4",
   sections: window.sectionsData || [],
   checkboxes: window.checkboxesData || {},
   information: window.informationData || {},
