36379711df559225f75a148d176c07efcee0203b
Juvia Suisei <13936649+juviasuisei@users.noreply.github.com>
2025-07-15 02:32:38 +0000

    bugfix

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Main data aggregator - combines data from separate files
 window.checklistData = {
-  version: "0.19.3",
+  version: "0.19.4",
   sections: window.sectionsData || [],
   checkboxes: window.checkboxesData || {},
   information: window.informationData || {},
